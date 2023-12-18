1bc80e5bc952a20f930a58a79e781eeabdcc1930
Goyal-Puja <goyalpuja48@gmail.com>
2023-12-18 13:34:12 +0530

    added

---
+++ src/linkedlist/AddOne.java
@@ -0,0 +1,56 @@
+package linkedlist;
+
+public class AddOne {
+    public static ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    public static ListNode addOneToLL(ListNode head){
+        ListNode head1 = reverse(head);
+        ListNode curr = head1;
+        while(curr != null){
+            if(curr.val + 1 <= 9){
+                curr.val = curr.val+1;
+                break;
+            } else {
+                curr.val = 0;
+                if(curr.next != null){
+                    curr.next = new ListNode(1);
+                    break;
+                }
+              curr = curr.next;
+            }
+        }
+         return reverse(head1);
+    }
+
+    public void print(ListNode head){
+        ListNode curr = head;
+        while(curr != null){
+            System.out.print(curr.val+"->");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args){
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        AddOne addOne = new AddOne();
+        ListNode result = addOne.addOneToLL(head);
+        addOne.print(result);
+    }
+}
