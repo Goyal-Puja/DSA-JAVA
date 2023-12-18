74cedf6bdd7544125b22dd1f2a56fe1670f685f7
Goyal-Puja <goyalpuja48@gmail.com>
2023-12-18 14:01:25 +0530

    added

---
+++ src/linkedlist/DetectALoop.java
@@ -0,0 +1,42 @@
+package linkedlist;
+
+public class DetectALoop {
+    public static boolean hasCycle(ListNode head){
+        if(head == null || head.next == null)
+            return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node2;  // creating a loop
+
+        DetectALoop loop = new DetectALoop();
+        if (loop.hasCycle(node1) == true){
+            System.out.println("Loop detected in the linked list");
+        } else {
+            System.out.println("No loop detected in the linked list");
+        }
+    }
+}
