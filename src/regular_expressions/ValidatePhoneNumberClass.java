7c2cec1dfa35a5096b76e84cb12bbd429f51baac
Goyal-Puja <goyalpuja48@gmail.com>
2025-01-25 22:44:58 +0530

    added

---
+++ src/regular_expressions/ValidatePhoneNumberClass.java
@@ -0,0 +1,26 @@
+package regular_expressions;
+
+public class ValidatePhoneNumberClass {
+
+    boolean validatePhoneNo(String mobileNumber){
+        boolean valid = true;
+        int length = mobileNumber.length();
+        if(length == 10){
+            for(int i=0;i<length;i++){
+                if(Character.isDigit(mobileNumber.charAt(i)) != true){
+                    valid = false;
+                    break;
+                }
+            }
+        } else {
+            valid = false;
+        }
+        return valid;
+    }
+
+    public static void main(String[] args){
+         ValidatePhoneNumberClass vp = new ValidatePhoneNumberClass();
+         boolean res = vp.validatePhoneNo("9876543210");
+         System.out.println(res);
+    }
+}
